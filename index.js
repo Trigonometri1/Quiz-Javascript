808f1688cb2151971876cedc5667e3d7b13fc617
Trigonometri1 <56674309+Trigonometri1@users.noreply.github.com>
2023-03-07 02:17:48 +0300

    Timer added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const options = quiz_card.querySelectorAll(".option_list .option");
 const time_second = quiz_card.querySelector(".timer .second_txt");
 const score_card = document.querySelector(".score_card");
 const current_question = document.querySelector(".current_question");
+const timer = document.querySelector(".quiz_card .timer .second_txt");
 
 // `<span><p>${question_number + 1} </p>of<p> ${
 //   questions.length
@@ -29,6 +30,7 @@ restart_btn.onclick = () => {
   quiz_rules.classList.remove("show_quiz_rules");
   quiz_card.classList.add("show_quiz_card");
   showQuestion(0);
+  startTimer();
 };
 
 let question_number = 0;
@@ -38,6 +40,8 @@ next_btn.onclick = () => {
   if (question_number < questions.length - 1) {
     question_number++;
     showQuestion(question_number);
+    clearInterval();
+    startTimer();
   } else {
     score_card.classList.add("show_score_card");
     quiz_card.classList.remove("show_quiz_card");
@@ -48,11 +52,6 @@ next_btn.onclick = () => {
 const showQuestion = (question_index) => {
   const question_txt = document.querySelector(".question_txt");
   const option_list = document.querySelector(".option_list");
-
-  let currentQuestionHTML = `
-    <span><p>${questions[question_number].id} </p>of<p> 
-      ${questions.length}
-    </p></span>`;
 
   let queHtml = `<span>${questions[question_index].question}</span>`;
   let optionHtml = `
@@ -74,5 +73,27 @@ const showQuestion = (question_index) => {
 
   question_txt.innerHTML = queHtml;
   option_list.innerHTML = optionHtml;
+
+  showCurrentQuestion();
+};
+
+const showCurrentQuestion = () => {
+  let currentQuestionHTML = `
+    <span><p>${questions[question_number].id} </p>of<p> 
+      ${questions.length}
+    </p></span>`;
   current_question.innerHTML = currentQuestionHTML;
+};
+
+//* Create a Timer
+const startTimer = () => {
+  let time = 20;
+  const timerInterval = setInterval(() => {
+    if (time <= 0) {
+      clearInterval(timerInterval);
+    } else {
+      timer.innerHTML = time;
+    }
+    time -= 1;
+  }, 1000);
 };
