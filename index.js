00d2782e37d4c3232f44d956728e4879ba776584
Trigonometri1 <56674309+Trigonometri1@users.noreply.github.com>
2023-03-07 01:45:46 +0300

    Question data added, Quiz card become dinamic

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
-console.log("Hello Quiz App");
+// declareting the elements
+const start_btn = document.querySelector(".start_btn");
+const quiz_rules = document.querySelector(".quiz_rules");
+const quit_btn = quiz_rules.querySelector(".buttons .quit");
+const restart_btn = quiz_rules.querySelector(".buttons .restart");
+const quiz_card = document.querySelector(".quiz_card");
+const next_btn = quiz_card.querySelector(".next_button");
+const options = quiz_card.querySelectorAll(".option_list .option");
+const time_second = quiz_card.querySelector(".timer .second_txt");
+const score_card = document.querySelector(".score_card");
+const current_question = document.querySelector(".current_question");
+
+// `<span><p>${question_number + 1} </p>of<p> ${
+//   questions.length
+// }</p></span>`;
+
+//* Start Button Click
+start_btn.onclick = () => {
+  quiz_rules.classList.add("show_quiz_rules");
+};
+
+//* Start Quit Click
+quit_btn.onclick = () => {
+  quiz_rules.classList.remove("show_quiz_rules");
+};
+
+//* Quiz-Start Button Click
+restart_btn.onclick = () => {
+  quiz_rules.classList.remove("show_quiz_rules");
+  quiz_card.classList.add("show_quiz_card");
+  showQuestion(0);
+};
+
+let question_number = 0;
+
+//*Next Button Click
+next_btn.onclick = () => {
+  if (question_number < questions.length - 1) {
+    question_number++;
+    showQuestion(question_number);
+  } else {
+    score_card.classList.add("show_score_card");
+    quiz_card.classList.remove("show_quiz_card");
+  }
+};
+
+//*Get Questions and Options Dinamicly
+const showQuestion = (question_index) => {
+  const question_txt = document.querySelector(".question_txt");
+  const option_list = document.querySelector(".option_list");
+
+  let currentQuestionHTML = `
+    <span><p>${questions[question_number].id} </p>of<p> 
+      ${questions.length}
+    </p></span>`;
+
+  let queHtml = `<span>${questions[question_index].question}</span>`;
+  let optionHtml = `
+    <div class="option">
+      <span>${questions[question_index].options[0]}</span>
+    </div>
+
+    <div class="option">
+      <span>${questions[question_index].options[1]}</span>
+    </div>
+
+    <div class="option">
+      <span>${questions[question_index].options[2]}</span>
+    </div>
+
+    <div class="option">
+      <span>${questions[question_index].options[3]}</span>
+    </div>`;
+
+  question_txt.innerHTML = queHtml;
+  option_list.innerHTML = optionHtml;
+  current_question.innerHTML = currentQuestionHTML;
+};
---
+++ questions.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    id: 1,
+    question: "Question One",
+    options: ["Q1 Option 1", "Q1 Option 2", "Q1 Option 3", "Q1 Option 4"],
+    answer: "General purpose app, specialized app, and mobile app",
+  },
+  {
+    id: 2,
+    question: "Question Two",
+    options: ["Q2 Option 1", "Q2 Option 2", "Q2 Option 3", "Q2 Option 4"],
+    answer: "Ribbons, tabs, and Galleries",
+  },
+  {
+    id: 3,
+    question: "Question Three",
+    options: ["Q3 Option 1", "Q3 Option 2", "Q3 Option 3", "Q3 Option 4"],
+    answer: "General purpose app, specialized app, and mobile app",
+  },
+  {
+    id: 4,
+    question: "Question Four",
+    options: ["Q4 Option 1", "Q4 Option 2", "Q4 Option 3", "Q4 Option 4"],
+    answer: "Microsoft word, excel, and powerpoint",
+  },
+  {
+    id: 5,
+    question: "Question Five",
+    options: ["Q5 Option 1", "Q5 Option 2", "Q5 Option 3", "Q5 Option 4"],
+    answer: "Microsoft word, excel, and powerpoint",
+  },
+];
